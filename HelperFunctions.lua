b5a67af8ade37a44e4289679189abdc708f7220a
Joshua Beers <beerman.develops@gmail.com>
2022-05-02 12:51:10 -0500

    Initialize Equipment Map
    Add Base Equipment Map
    Add get_player_buffs function
    Update calculate_enfeebling_duration function.

--- HelperFunctions.lua
+++ HelperFunctions.lua
@@ -1,3 +1,5 @@
+equip_slots = {'main','sub','range','ammo','head','neck','left_ear','right_ear','body','hands','left_ring','right_ring','back','waist','legs','feet'}
+
 function calculate_enhancing_duration(spell, equipment)
 
 end
@@ -19,16 +21,73 @@ function calculate_enfeebling_duration(spell, equipment)
         + (player.job_points[player.main_job].enfeebling_magic_duration or 0)
         + (buffs.stymie and (player.job_points[player.main_job].stymie_effect or 0))
 
-    for _, slot in ipairs(slots) do
-        local item = windower.ffxi.get_items(equipment[slot .. '_bag'], gear_info[slot])
+    for _, slot in ipairs(equip_slots) do
+        local item = windower.ffxi.get_items(equipment[slot .. '_bag'], equipment[slot]).id
+        local modifiers = enfeebling_modifiers[item.id]
+
+        if modifiers then
+            for index, value in pairs(modifiers) do
+                if index == 1 then
+                    duration_modifier = duration_modifier + value
+                elseif index == 'augment' then
+                    augment_duration_modifier = augment_duration_modifier + value
+                elseif index == 'saboteur' and buffs.saboteur then
+                    saboteur_modifier = saboteur_modifier + value
+                end
+            end
+        end
+
+        if enfeebling_relic_bonus[item.id] then
+            duration_bonus = duration_bonus + (player.merits.enfeebling_magic_duration and (player.merits.enfeebling_magic_duration * 6) or 0)
+        end
+
+        if composure_gear[item.id] then
+            composure_count = composure_count + 1
+        end
     end
+
+    local duration = math.floor(math.floor(math.floor((math.floor(base_duration * saboteur_modifier) + duration_bonus) * duration_modifier) * augment_duration_modifier) * composure_modifier)
+    local duration_map = enfeebling_resist_states:map(
+        function(resist_multiplier)
+            return math.floor(duration * resist_multiplier)
+        end
+    )
+
+    return duration_map
 end
 
 function get_basic_info(spell, equipment)
     local spell_info = windower.res.spells[spell.recast_id]
     local equipment = equipment or windower.ffxi.get_items('equipment')
     local player = windower.ffxi.get_player()
-    local buffs = player.buffs
+    local buffs = get_player_buffs(player)
 
     return spell_info, equipment, player, buffs
+end
+
+function update_gear_map()
+    -- TODO: Look at inventory and update augmented gear maps
+end
+
+function get_player_buffs(player)
+    player = player or windower.ffxi.get_player()
+    if not player or not player.buffs then return {} end
+    local buffs = T{}
+    for _, buff_id in pairs(player.buffs) do
+        buffs[buff_id] = (buffs[buff_id] or 0) + 1
+        local buff_info = res.buffs[buff_id]
+        if buff_info then
+            local name = buff_info.english:lower()
+            buffs[name] = (buffs[name] or 0) + 1
+        end
+    end
+    setmetatable(buffs, {
+        __index = function(t, key)
+            if key and type(key) == 'string' then
+                key = key:lower()
+            end
+            return rawget(t, key)
+        end
+    })
+    return buffs
 end
---
+++ data/equipment_map.lua
@@ -0,0 +1,108 @@
+enfeebling_modifiers = {
+    [11108] = {saboteur=0.10}, -- Estoqueur's Gantherots +2
+    [11208] = {saboteur=0.05}, -- Estoqueur's Gantherots +1
+    [25441] = {augment=0.15}, -- Duelist's Torque
+    [25442] = {augment=0.2}, -- Duelist's Torque +1
+    [25443] = {augment=0.25}, -- Duelist's Torque +2
+    [25827] = {0.2}, -- Regal Cuffs
+    [26109] = {0.1}, -- Snotra Earring
+    [26188] = {0.1}, -- Kishar Ring
+    [26363] = {0.05}, -- Obstinate Sash
+    [27060] = {saboteur=0.11}, -- Lethargy Gantherots
+    [27061] = {saboteur=0.12}, -- Lethargy Gantherots +1
+}
+
+enfeebling_resist_states = {
+    [0] = 1,
+    [1] = 0.5,
+    [2] = 0.25,
+    [3] = 0.125,
+}
+
+-- Default augment assumes max
+enhancing_modifiers = {
+    [11123] = {perpetuance=0.50}, -- Savant's Bracers +2
+    [11223] = {perpetuance=0.25}, -- Savant's Bracers +1
+    [11148] = {0.2}, -- Estoqueur's Houseaux +2
+    [11248] = {0.1}, -- Estoqueur's Houseaux +1
+    [20677] = {augment=0.04}, -- Colada
+    [21072] = {augment=0.06}, -- Gada
+    [22054] = {augment=0.1}, -- Grioavolr
+    [22055] = {0.1}, -- Oranyan
+    [22097] = {0.1}, -- Argute Staff
+    [22098] = {0.15}, -- Pedagogy Staff
+    [22099] = {0.2}, -- Musa
+    [23134] = {0.1}, -- Vitiation Tabard +2
+    [23149] = {0.08}, -- Pedagogy Gown +2
+    [23178] = {0.18}, -- Atrophy Gloves +2
+    [23285] = {0.25}, -- Futhark Trousers +2
+    [23469] = {0.15}, -- Vitiation Tabard +3
+    [23484] = {0.12}, -- Pedagogy Gown +3
+    [23513] = {0.2}, -- Atrophy Gloves +3
+    [23620] = {0.3}, -- Futhark Trousers +3
+    [25441] = {augment=0.15}, -- Duelist's Torque
+    [25442] = {augment=0.2}, -- Duelist's Torque +1
+    [25443] = {augment=0.25}, -- Duelist's Torque +2
+    [26250] = {0.2}, -- Sucellos's Cape
+    [26354] = {0.1}, -- Embla Sash
+    [26419] = {0.1}, -- Ammurapi Shield
+    [26736] = {augment=0.1}, -- Telchine Cap
+    [26782] = {0.1}, -- Erilaz Galea
+    [26783] = {0.15}, -- Erilaz Galea +1
+    [26894] = {augment=0.1,Regen=12}, -- Telchine Chasuble
+    [27048] = {augment=0.1}, -- Telchine Gloves
+    [27090] = {perpetuance=0.50}, -- Arbatel Bracers
+    [27091] = {perpetuance=0.55}, -- Arbatel Bracers +1
+    [27194] = {0.1}, -- Futhark Trousers
+    [27195] = {0.2}, -- Futhark Trousers +1
+    [27235] = {augment=0.1}, -- Telchine Braconi
+    [27405] = {augment=0.1}, -- Telchine Pigaches
+    [27419] = {0.25}, -- Lethargy Houseaux
+    [27420] = {0.3}, -- Lethargy Houseaux +1
+    [27639] = {augment_received=0.1}, -- Ajax +1
+    [27947] = {0.15}, -- Atrophy Gloves
+    [27968] = {0.16}, -- Atrophy Gloves +1
+    [28621] = {augment=0.2}, -- Ghostfyre Cape
+    [28638] = {embolden=0.15}, -- Evasionist's Cape -- Default augment assumes max
+}
+
+composure_modifiers = {
+    [2] = {0.1},
+    [3] = {0.2},
+    [4] = {0.35},
+    [5] = {0.5}
+}
+
+composure_gear = S{
+    11068,
+    11088,
+    11108,
+    11128,
+    11148,
+    26748,
+    26749,
+    26906,
+    26907,
+    27060,
+    27061,
+    27245,
+    27246,
+    27419,
+    27420,
+}
+
+enfeebling_relic_bonus = S{
+    10654,
+    23067,
+    23402,
+    26632,
+    26633,
+}
+
+enhancing_relic_bonus = S{
+    10694,
+    23201,
+    23536,
+    26984,
+    26985,
+}
