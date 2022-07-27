5f1c1ea862bfd52e0c2ae3cddeaee654ccbd1391
Stromgarde <epollino@gmail.com>
2022-07-27 11:49:06 -0500

    Additional BLU spells added

--- static/resource_modifications.lua
+++ static/resource_modifications.lua
@@ -32,23 +32,43 @@ return {
     },
 
     spells = {
+        [515] = {duration=60,status=136,unconfirmed=true}, -- Maelstrom
+        [534] = {duration=120,status=12,unconfirmed=true}, -- Mysterious Light
+        [536] = {duration=30,status=3,unconfirmed=true}, -- Poison Breath
+        [539] = {duration=60,status=147,unconfirmed=true}, -- Terror Touch
         [547] = {duration=90,status=93,blu_points=1}, -- Cocoon
         [548] = {duration=90,status=565,blu_points=3}, -- Filamented Hold  
+        [555] = {duration=120,status=12,unconfirmed=true}, -- Magnetite Cloud
+        [565] = {duration=90,status=13,unconfirmed=true}, -- Radiant Breath  also status=6
         [582] = {duration=120,status=6,blu_points=2}, -- Chaotic Eye
         [584] = {duration=60,status=2,blu_points=2}, -- Sheep Song
+        [596] = {duration=60,status=2,unconfirmed=true}, -- Pinecone Bomb  Duration of effect varies with TP
+        [599] = {duration=180,status=3,unconfirmed=true}, -- Queasyshroom  - Duration of effect varies with TP
         [603] = {duration=90,status=138,blu_points=3, unconfirmed=true}, -- Wild Oats
+        [608] = {duration=180,status=4,unconfirmed=true}, -- Frost Breath
+        [611] = {duration=180,status=3,unconfirmed=true}, -- Disseverment
+        [623] = {duration=5,status=10,unconfirmed=true}, -- Head Butt
+        [628] = {duration=5,status=10,unconfirmed=true}, -- Frypan
+        [634] = {duration=30,status=5,unconfirmed=true}, -- Light of Penance  also status=11
         [644] = {duration=90,status=4,overwrites={644},blu_points=4, unconfirmed=true}, -- Mind Blast
         [647] = {duration=900,status=36,blu_points=2}, -- Zephyr Mantle
         [648] = {duration=30,status=7,overwrites={258,648},blu_points=1, unconfirmed=true}, -- Regurgitation  
+        [650] = {duration=120,status=149,unconfirmed=true}, -- Seedspray
+        [651] = {duration=90,status=147,unconfirmed=true}, -- Corrosive Ooze   -- also status=149
+        [652] = {duration=60,status=146,unconfirmed=true}, -- Spiral Spin
+        [654] = {duration=180,status=4,unconfirmed=true}, -- Sub-zero Smash
+        [656] = {duration=120,status=167,unconfirmed=true}, -- Acrid Stream
         [660] = {duration=90,status=565,overwrites={548},blu_points=3}, -- Cimicine Discharge  
         [661] = {duration=300,status=33,blu_points=5}, -- Animating Wail
         [662] = {duration=300,status=43,blu_points=3}, -- Battery Charge
         [668] = {duration=900,status=152,blu_points=3}, -- Magic Barrier
         [671] = {duration=60,status=5,blu_points=4}, -- Auroral Drape; also status 6
         [674] = {duration=180,status=45,blu_points=1}, -- Fantod
+        [675] = {duration=60,status=5,unconfirmed=true}, -- Thermal Pulse
         [679] = {duration=300,status=36,blu_points=3}, -- Occultation
         [682] = {duration=60,status=31,overwrites={682},blu_points=2, unconfirmed=true}, -- Delta Thrust
         [685] = {duration=180,status=116,blu_points=3}, -- Barrier Tusk
+        [687] = {duration=90,status=6,unconfirmed=true}, -- Water Bomb
         [692] = {duration=8,status=10,blu_points=4, unconfirmed=true}, -- Sudden Lunge
         [696] = {duration=180,status=61,blu_points=5}, -- O. Counterstance
         [699] = {duration=120,status=146,overwrites={699,742},blu_points=2, unconfirmed=true}, -- Barbed Crescent
@@ -56,6 +76,7 @@ return {
         [703] = {duration=180,status=565,overwrites={548,660,703},blu_points=6, unconfirmed=true}, -- Embalming Earth
         [704] = {duration=60,status=4,overwrites={644,704},blu_points=6, unconfirmed=true}, -- Paralyzing Triad
         [705] = {duration=180,status=133,overwrites={705},blu_points=3, unconfirmed=true}, -- Foul Waters  
+        [707] = {duration=15,status=156,unconfirmed=true}, -- Retinal Glare
         [708] = {duration=90,status=12,overwrites={708},blu_points=6, unconfirmed=true}, -- Subduction
         [710] = {duration=300,status=33,blu_points=6}, -- Erratic Flutter
         [716] = {duration=30,status=540,overwrites={716},blu_points=6, unconfirmed=true}, -- Nectarous Deluge
@@ -79,6 +100,23 @@ return {
         [746] = {duration=12,status=28}, -- Blistering Roar
         [750] = {duration=180,status=604}, -- Mighty Guard
         [751] = {duration=60,status=15}, -- Cruel Joke
-        [752] = {duration=60,status=31,overwrites={682,752}, unconfirmed=true} -- Cesspool
+        [752] = {duration=60,status=31,overwrites={682,752}, unconfirmed=true}, -- Cesspool
+        [753] = {duration=60,status=167,unconfirmed=true}, -- Tearing Gust
+
+        --[[ Need duration testing: 
+        [524] = {duration=,status=146,unconfirmed=true}, -- Sandspin
+        [531] = {duration=,status=11,unconfirmed=true}, -- Ice Break
+        [532] = {duration=,status=10,unconfirmed=true}, -- Blitzstrahl
+        [563] = {duration=,status=5,unconfirmed=true}, -- Hecatomb Wave
+        [597] = {duration=,status=13,unconfirmed=true}, -- Sprout Smack  Duration of effect varies with TP
+        [618] = {duration=,status=11,unconfirmed=true}, -- Blastbomb
+        [620] = {duration=,status=137,unconfirmed=true}, -- Battle Dance  Duration of effect varies with TP
+        [638] = {duration=,status=3,unconfirmed=true}, -- Feather Storm
+        [640] = {duration=,status=10,unconfirmed=true}, -- Tail Slap
+        [721] = {duration=,status=10,unconfirmed=true}, -- Anvil Lightning
+        [725] = {duration=,status=156,unconfirmed=true}, -- Blinding Fulgor
+        [749] = {duration=,status=11,unconfirmed=true}, -- Polar Roar
+        ]]
+
   }
 }
